37b5869b4ea50204a53473d3d434198980c19d9f
efaruk <faruk.pehlivanli@gmail.com>
2014-12-04 09:22:48 +0200

    some refactoring, nothing important...

--- UnlockedStateProvider/UnlockedStateProvider/UnlockedExtensions.cs
+++ UnlockedStateProvider/UnlockedStateProvider/UnlockedExtensions.cs
@@ -8,7 +8,7 @@ using System.Web.Mvc;
 
 namespace UnlockedStateProvider
 {
-	public static class ContextHelper
+	public static class UnlockedExtensions
 	{
 
 		public static IUnlockedStateStore GetStoreFromContext(this ControllerContext context)
@@ -146,6 +146,34 @@ namespace UnlockedStateProvider
 				result = context.Request.Cookies[cookieName].Value;
 			}
 			return result;
+		}
+
+		public static void StartSessionIfNew(this HttpContext context)
+		{
+			if (context != null && context.Session != null &&
+				context.Session.IsNewSession && !context.Session.IsReadOnly)
+			{
+				context.Session[UnlockedStateUsageAttribute.SESSION_STARTED_KEY] = true;
+			}
+		}
+
+		public static void StartSessionIfNew(this ControllerContext context)
+		{
+			if (context != null && context.HttpContext != null && context.HttpContext.Session != null &&
+				context.HttpContext.Session.IsNewSession && !context.HttpContext.Session.IsReadOnly)
+			{
+				context.HttpContext.Session[UnlockedStateUsageAttribute.SESSION_STARTED_KEY] = true;
+
+			}
+		}
+
+		public static void StartSessionIfNew(this Controller controller)
+		{
+			if (controller != null && controller.HttpContext != null && controller.HttpContext.Session != null &&
+				controller.HttpContext.Session.IsNewSession && !controller.HttpContext.Session.IsReadOnly)
+			{
+				controller.HttpContext.Session[UnlockedStateUsageAttribute.SESSION_STARTED_KEY] = true;
+			}
 		}
 
 	}
