f4ef86692ec85e8b6d653c3075932094d4c1fab0
efaruk <faruk.pehlivanli@gmail.com>
2014-12-16 14:11:29 +0200

    forgetted

---
+++ Sifu/Sifu/Dialogs/DialogConnectServer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using StackExchange.Redis;
+
+namespace Sifu.Dialogs
+{
+	public partial class DialogConnectServer : Form
+	{
+		public DialogConnectServer()
+		{
+			InitializeComponent();
+		}
+
+		private ConnectionMultiplexer _redisConnection;
+
+		private void DialogConnectServer_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void btnCancel_Click(object sender, EventArgs e)
+		{
+			this.DialogResult = DialogResult.Cancel;
+			this.Hide();
+		}
+
+		private void btnOK_Click(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
---
+++ Sifu/Sifu/SharedConnection.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StackExchange.Redis;
+
+namespace Sifu
+{
+	public class SharedConnection
+	{
+		private static readonly Lazy<SharedConnection> instance = new Lazy<SharedConnection>(() => new SharedConnection());
+		private static ConnectionMultiplexer _redisConnection;
+
+		private SharedConnection()
+		{
+			
+		}
+
+		public static SharedConnection Instance
+		{
+			get { return instance.Value; }
+		}
+
+		public void Connect(string host)
+		{
+			
+		}
+	}
+}
