8dfbae661cad444038b10270cb43c4f7d81a4b23
efaruk <faruk.pehlivanli@gmail.com>
2015-02-10 01:50:33 +0200

    Multicast project added

---
+++ Multicasting/Multicasting.Web/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+
+namespace Multicasting.Web.Controllers {
+	public class HomeController : Controller {
+		public ActionResult Index() {
+			ViewBag.Message = "Hoşgeldin birader...";
+			Session["init"] = 1;
+			foreach (string serverVariable in Request.ServerVariables) {
+				Session[serverVariable] = Request[serverVariable];
+			}
+			return View();
+		}
+
+	}
+}
---
+++ Multicasting/Multicasting/MulticastSender.cs
@@ -0,0 +1,18 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace Multicasting
+{
+	public class MulticastSender
+	{
+		private IPAddress _ipAddress = IPAddress.Parse("224.0.0.1");
+
+		void Test()
+		{
+			var socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+			var client = new UdpClient(AddressFamily.InterNetwork);
+			client.JoinMulticastGroup(_ipAddress, 1);
+			
+		}
+	}
+}
