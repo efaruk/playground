0495872a18ed2ce53c184dcf6a752b095ab46d28
efaruk <faruk.pehlivanli@gmail.com>
2015-02-19 13:13:57 +0200

    Tercuman initial state

---
+++ Tercuman/Tercuman.Website/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+using System.Web.SessionState;
+
+namespace Tercuman.Website.Controllers
+{
+	[SessionState(SessionStateBehavior.Disabled)]
+	public class HomeController : Controller
+	{
+		// GET: Home
+		public ActionResult Index()
+		{
+			return View();
+		}
+	}
+}
---
+++ Tercuman/Tercuman.Website/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Tercuman.Website.MvcApplication" Language="C#" %>
