0fa4d4341a4bb19080d389792fed89f246781df8
tamerboncooglu <tamerboncooglu@gmail.com>
2015-02-19 15:42:29 +0200

    web api (glob service) added

---
+++ Tercuman/Tercuman.Service/Controllers/ValuesController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Mvc;
+using Tercuman.Service.Data;
+using Tercuman.Service.Models;
+
+namespace Tercuman.Service.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        public GlobDBEntities Context = new GlobDBEntities();
+
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            var result = new List<string>();
+
+            var langs = Context.Content.Select(w => w.LangCode).Distinct();
+
+            foreach (var item in langs)
+            {
+                result.Add(item);
+            }
+
+            return result.ToList();
+        }
+
+        // GET api/values/5
+        //public ContentResponse Get(ContentRequest request)
+        //{
+        //    return new ContentResponse
+        //    {
+        //        Value = "Deneme"
+        //    };
+        //}
+
+        // POST api/values
+        //Model {"LangCode":"en-us","TableName":"Product","FieldName":"ProductName"}
+        public ContentResponse Post([FromBody]ContentRequest request)
+        {
+            var result = new ContentResponse();
+            var content = Context.Content.FirstOrDefault(
+                w =>
+                    w.FieldName == request.FieldName && w.LangCode == request.LangCode &&
+                    w.TableName == request.TableName);
+            if (content != null)
+                result.Value = content.Value;
+            return result;
+        }
+
+        // PUT api/values/5
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
+
+        //// DELETE api/values/5
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
---
+++ Tercuman/Tercuman.Service/Models/ContentRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Tercuman.Service.Models
+{
+    public class ContentRequest
+    {
+        public string LangCode { get; set; }
+        public string TableName { get; set; }
+        public string FieldName { get; set; }
+    }
+}
