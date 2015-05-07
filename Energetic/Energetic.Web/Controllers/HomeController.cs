7bbce647aba45f914148f15e4ea3f0ab9e014548
efaruk <faruk.pehlivanli@gmail.com>
2015-05-07 18:05:03 +0300

    Energetic Dapper working

---
+++ Energetic/Energetic.Web/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Energetic.Data.Entities;
+using Energetic.DataAccess.Repository;
+
+namespace Energetic.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static readonly string ConnectionString = ConfigurationManager.ConnectionStrings["SqliteConnection"].ConnectionString;
+
+        public HomeController()
+        {
+        }
+
+        public ActionResult Index()
+        {
+            Customer customer = null;
+            using (var repository = new CustomerRepository(ConnectionString))
+            {
+                customer = repository.GetCustomer("C0A453A2-CAED-47DB-9A87-4E57A68C247C");
+            }
+            return View(customer);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Energetic/Energetic.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Energetic.Web.MvcApplication" Language="C#" %>
